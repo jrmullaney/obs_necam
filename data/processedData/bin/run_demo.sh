66c679d32dc3629bbbf647d81c886e92d9d977fb
James Mullaney <j.mullaney@sheffield.ac.uk>
2020-10-13 22:57:30 +0100

    Added data/processedData/bin

---
+++ data/processedData/bin/run_demo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This should already be set, but doesn't harm to make sure:
+export DYLD_LIBRARY_PATH=${LSST_LIBRARY_PATH}
+
+# If developing your own gen3 obs_package, it helps to start with a clean
+# DATA_REPO each time:
+if [ -f DATA_REPO/butler.yaml ]; then
+    rm -rf DATA_REPO
+fi
+
+# If DATA_REPO does not exist, create it and register your instrument.
+if [ ! -f DATA_REPO/butler.yaml ]; then
+    butler create DATA_REPO
+    butler register-instrument DATA_REPO lsst.obs.necam.NeCam
+fi
+
+# Import the reference catalogues to the butler.
+butler import DATA_REPO "${PWD}/input_data" --export-file "${PWD}/input_data/export.yaml" --skip-dimensions instrument,physical_filter,detector
+
+if [ ! -d DATA_REPO/NeCam/raw ]; then
+    butler ingest-raws DATA_REPO ../rawData/ --ingest-task lsst.obs.necam.ingest.NeCamRawIngestTask
+    butler define-visits DATA_REPO --instrument lsst.obs.necam.NeCam --collections NeCam/raw/all
+fi
+
+pipetask run -d "exposure=1" -b DATA_REPO/butler.yaml --input NeCam/raw/all --register-dataset-types -p "${PIPE_TASKS_DIR}/pipelines/ProcessCcd.yaml" --instrument lsst.obs.necam.NeCam --output-run demo_collection
