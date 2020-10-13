a24259e3fcc7e6a49f24851d194dd4eaaf99aaf4
James Mullaney <j.mullaney@sheffield.ac.uk>
2020-10-14 00:03:05 +0100

    Edited runLsstDocker to run using specific weekly tag

--- data/runLsstDocker_vWeekly.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -ti \
-    -v ${PWD}:/home/lsst/data/ \
-    -v /Users/James/Work/LSST/obs_necam:/opt/lsst/software/stack/stack/current/Linux64/obs_necam \
-    lsst:weekly
-        
